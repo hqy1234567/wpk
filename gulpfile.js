f137823eae2561c2bba51e7f1f672e3a4f3584be
huangqingyan <2990986131@qq.com>
2020-12-21 10:02:51 +0800

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+const $ = require('jquery');
+// const swiper = require('swiper');
+
+function banner() {
+    console.log($(".clear"));
+    /* var swiper = new Swiper('.swiper-container', {
+        spaceBetween: 30,
+        centeredSlides: true,
+        loop: true,
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+
+    // 鼠标移入，轮播停止，移出继续轮播
+    $('.swiper-container').hover(function () {
+        swiper.autoplay.stop();
+    }, function () {
+        swiper.autoplay.start();
+    }); */
+
+    
+}
+exports.banner = bsnner;
